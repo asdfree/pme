07634a604bf013ba7c5dcef590c4e7d3401f6441
Anthony Damico <ajdamico@gmail.com>
2017-07-23 02:09:48 -0400

    sampling to keep build within restrictions

--- tests/setup.R
+++ tests/setup.R
@@ -1,4 +1,15 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
 library(lodown)
-lodown( "pme" , output_dir = file.path( getwd() ) )
+
+pme_cat <-
+	get_catalog( "pme" ,
+		output_dir = file.path( getwd() ) )
+
+# sample 75% of the records
+which_records <- sample( seq( nrow( pme_cat ) ) , round( nrow( pme_cat ) * 0.75 ) )
+
+# always sample year == 2016
+pme_cat <- unique( pme_cat[ which_records , ] , subset( pme_cat , year == 2016 ) )
+
+lodown( "pme" , pme_cat )
